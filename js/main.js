a9e7cfe780886fd6c2673a4153338c4ff1013918
Book12 <Book12@lopan.loc>
2017-10-02 17:50:00 +0300

    fine version

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+  $(".owl-carousel-1").owlCarousel({
+  	items:1,
+  	nav:true
+  });
+
+  $(".owl-carousel-2").owlCarousel({
+  	items:5,
+    nav:true,
+    autoplay:true,
+    loop: true,
+    dots:false    
+  });
+
+  $(".js_height").matchHeight();
+  
+});
